c19c6d3b98207edd55f1e1adb1a72bf4e64aa15d
Hisen Zhang <hisen@hisenz.com>
2021-06-02 22:24:11 -0400

    schedule now on separate thread

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import headless
 import argparse
-import sys
-import time
-import logging
 import threading
+import schedule
+import sys
+import logging
 import py_cui
 from modules.tui import TUI
 from modules.config import STATION_NAME
 
 
-# def backgroundTask(station):
-#     while station.mixer.get_init():
-#         logging.debug("Hello from backgroud thread...")
-#         schedule.run_pending()
-#         station.playControl.loop('show')
-#         time.sleep(1)
+def runSchedule(station):
+    while station.mixer.get_init():
+        logging.debug("Hello from backgroud thread...")
+        schedule.run_pending()
 
 
 def main():
@@ -30,16 +28,13 @@ def main():
         try:
             frame = None
             mainWindow = py_cui.PyCUI(4, 3, exit_key=1)
-            mainWindow.set_refresh_timeout(1)            
+            mainWindow.set_refresh_timeout(1)
             mainWindow.set_title(
                 '{} Broadcast Automation System'.format(STATION_NAME))
             frame = TUI(mainWindow)
-            # daemonThread = threading.Thread(
-            #     name='Daemon', target=backgroundTask, args=(frame.station,), daemon=True)
-            # daemonThread.start()
-            # Wait until initial stationID is done
-            # while frame.mixer.channelMap['stationID'].get_busy():
-            #     time.sleep(0.5)
+            daemonThread = threading.Thread(
+                name='Daemon', target=runSchedule, args=(frame.station,), daemon=True)
+            daemonThread.start()
             logging.info("TUI starting...")
             mainWindow.start()
             logging.info("TUI exited.")
