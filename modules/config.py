ef1c234d8b2f2821aa117c1fbe057b2abfb62953
Hisen Zhang <hisen@hisenz.com>
2021-06-02 22:25:05 -0400

    allow user defined channels

--- modules/config.py
+++ modules/config.py
@@ -29,8 +29,6 @@ LIB_BASE = 'lib'
 #   AUDIO    #
 #------------#
 
-# number of channels in virtual mixer. 48 is more than sufficient.
-NUM_CHANNEL = 48
 # transition time in ms.
 TRANSITION_LENGTH = 1000
 # how much lower the audio should be when being surpressed (e.g. during a Station ID)
@@ -41,3 +39,5 @@ SOUND_FORMAT = ('.mp3', '.wav', '.ogg', 'm4a') # Basically everything ffmpeg sup
 BITRATE = '192k'
 # loudness in LUFS
 LOUDNESS = -23
+# User defined show type. Each type will use an individual channel
+USER_CHANNEL = [] # e.g. ['typeA','typeB']
