f025344ef2605db6cd70b447e8077514fd10f3bf
Hisen Zhang <zhangz29@rpi.edu>
2021-07-10 23:55:19 -0400

    doc

--- modules/station.py
+++ modules/station.py
@@ -12,13 +12,15 @@ from .util import configManager, fsUtil, db
 
 
 class manager:
+    """Station manager. The public interface for all operations. Include a play control instance.
+    """
     def __init__(self) -> None:
         self.mixer = audio.virtualMixerWrapper()
         self.playControl = play.control(self.mixer)
         self.cwd = os.getcwd()
         self.systemStat = None
         self.db = db()
-        self.lastSignIn = None
+        self.lastSignIn = None # identify if system has signed in successfully (not None)
         try:
             self.db.connect('db.json')
         except IOError as e:
@@ -37,17 +39,25 @@ class manager:
             self.mixer.pause()
 
     def scheduleInit(self):
+        """Register events in schedule
+        """
         cfg = configManager.cfg.schedule
+        # System monitor
         schedule.every(cfg.systemMonitor.interval).minutes.at(
             cfg.systemMonitor.time).do(self.systemMonitor)
+        # Station ID
         schedule.every(cfg.stationID.interval).hours.at(
             cfg.stationID.time).do(self.ID)
+        # Mixer digest
         schedule.every(cfg.mixerDigest.interval).minutes.at(
             cfg.mixerDigest.time).do(self.mixer.digest)
+        # Volume guard
         schedule.every(cfg.volumeGuard.interval).minutes.at(
             cfg.volumeGuard.time).do(self.mixer.volumeGuard)
 
     def scheduleRun(self):
+        """Run the schedule loop
+        """ 
         schedule.run_pending()
 
     # TODO signIn / Out special audio
@@ -72,6 +82,11 @@ class manager:
         self.lastSignIn = time.time()
 
     def loudNorm(self, targets: list = None):
+        """Loudness normalization in parallel
+
+        Args:
+            targets (list, optional): Sounds to be normalized. Defaults to None.
+        """
         procs = []
 
         if targets is None:
@@ -109,6 +124,11 @@ class manager:
                     p.kill()
 
     def systemMonitor(self):
+        """Monitor system resources such as CPU and RAM usage
+
+        Returns:
+            dict: dictionary containing system statistics
+        """ 
         try:
             self.systemStat = {
                 'CPU': psutil.cpu_percent(),
